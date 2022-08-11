89193d3422ca16d2c7c8c43ba8090f930aa96442
Pablo Silva Sousa <pablopablito156@gmail.com>
2022-08-11 15:49:33 -0300

    chamando o botão

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const inputEmail = document.querySelector('#inputEmail');
 const inputSenha = document.querySelector('#inputSenha');
+const botao = document.querySelector('#botao');
 
 botao.addEventListener('click', function () {
   if (inputEmail.value === 'tryber@teste.com' && inputSenha.value === '123456') {
@@ -7,5 +8,5 @@ botao.addEventListener('click', function () {
   }
   else {
     alert('Email ou senha inválidos.');
-  }
-  });
+  }}
+  );
