83c72265e678184fdc8bcbfc99d9a2b2e8461880
Pablo Silva Sousa <pablopablito156@gmail.com>
2022-08-11 16:14:06 -0300

    Resolvendo problemas Lint

--- script.js
+++ script.js
@@ -2,11 +2,8 @@ const inputEmail = document.querySelector('#inputEmail');
 const inputSenha = document.querySelector('#inputSenha');
 const botao = document.querySelector('#botao');
 
-botao.addEventListener('click', function () {
+botao.addEventListener('click', () => {
   if (inputEmail.value === 'tryber@teste.com' && inputSenha.value === '123456') {
     alert('Olá, Tryber!');
-  }
-  else {
-    alert('Email ou senha inválidos.');
-  }}
-  );
+  } else { alert('Email ou senha inválidos.'); }
+});
